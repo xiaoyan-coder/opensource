9ba2f570f4c7ea9b661271ef974ea6fedb359bb6
xiaoyan <2687477463@qq.com>
2020-08-26 08:28:37 +0800

    给定一个数组跟一个目标值，查看数组中的元素相加能否等于目标值，返回在数组中的下标

--- sumoftwonum.py
+++ sumoftwonum.py
@@ -12,10 +12,12 @@ nums[0] + nums[1] = 2 + 7 = 9
 from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # 设置两层循环，第一层循环，从第一个数字到倒数第二个数字。第一层循环中的每一个数字与相邻的数字相加一次
         for i in range(0,len(nums)-1):
+            # 第二层循环，从第二个数字(即i+1),一直比对到最后一个数字
             for j in range(i+1,len(nums)):
                 if nums[i]+nums[j]==target:
+                    # 如果有相等就返回下标
                     return [i,j]
-
 a=Solution().twoSum([2, 7, 11, 15],9)
 print(a)
